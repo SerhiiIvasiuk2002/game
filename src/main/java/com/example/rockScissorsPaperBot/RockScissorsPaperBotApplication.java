75e61d8838fbba97a1609c4c341408f58472cf9c
igor <igorkorolj@gmail.com>
2025-06-21 14:40:05 +0300

    first commit

---
+++ src/main/java/com/example/rockScissorsPaperBot/RockScissorsPaperBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.rockScissorsPaperBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockScissorsPaperBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockScissorsPaperBotApplication.class, args);
+	}
+
+}
